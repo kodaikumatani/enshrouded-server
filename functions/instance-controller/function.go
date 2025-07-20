fd177eee948fd897f8dd4a6f6f2043ed4a5b55fa
kodaikumatani <koudai.aco5423@gmail.com>
2025-07-21 03:15:57 +0900

    feat: InstanceController の実装

---
+++ functions/instance-controller/function.go
@@ -0,0 +1,94 @@
+package function
+
+import (
+	"context"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	compute "cloud.google.com/go/compute/apiv1"
+	"cloud.google.com/go/compute/apiv1/computepb"
+	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+)
+
+func init() {
+	functions.HTTP("InstanceController", handleRequest)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	path := r.URL.Path
+	if strings.HasSuffix(path, "/start") {
+		start(w, r)
+	} else if strings.HasSuffix(path, "/stop") {
+		stop(w, r)
+	} else {
+		http.Error(w, "Invalid path. Use /start or /stop", http.StatusBadRequest)
+	}
+}
+
+func start(w http.ResponseWriter, _ *http.Request) {
+	ctx := context.Background()
+
+	instancesClient, err := compute.NewInstancesRESTClient(ctx)
+	if err != nil {
+		log.Printf("An internal error occurred: %v", err)
+		http.Error(w, "An internal error occurred", http.StatusInternalServerError)
+		return
+	}
+	defer instancesClient.Close()
+
+	req := &computepb.StartInstanceRequest{
+		Project:  os.Getenv("PROJECT_ID"),
+		Zone:     os.Getenv("ZONE"),
+		Instance: os.Getenv("INSTANCE"),
+	}
+
+	op, err := instancesClient.Start(ctx, req)
+	if err != nil {
+		log.Printf("unable to start instance: %v", err)
+		http.Error(w, "unable to start instance", http.StatusInternalServerError)
+		return
+	}
+
+	if err = op.Wait(ctx); err != nil {
+		log.Printf("unable to wait for the operation to complete: %v", err)
+		http.Error(w, "unable to wait for the operation to complete", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func stop(w http.ResponseWriter, _ *http.Request) {
+	ctx := context.Background()
+
+	instancesClient, err := compute.NewInstancesRESTClient(ctx)
+	if err != nil {
+		log.Printf("An internal error occurred: %v", err)
+		http.Error(w, "An internal error occurred", http.StatusInternalServerError)
+		return
+	}
+	defer instancesClient.Close()
+
+	req := &computepb.StopInstanceRequest{
+		Project:  os.Getenv("PROJECT_ID"),
+		Zone:     os.Getenv("ZONE"),
+		Instance: os.Getenv("INSTANCE"),
+	}
+
+	op, err := instancesClient.Stop(ctx, req)
+	if err != nil {
+		log.Printf("unable to stop instance: %v", err)
+		http.Error(w, "unable to stop instance", http.StatusInternalServerError)
+		return
+	}
+
+	if err = op.Wait(ctx); err != nil {
+		log.Printf("unable to wait for the operation to complete: %v", err)
+		http.Error(w, "unable to wait for the operation to complete", http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
