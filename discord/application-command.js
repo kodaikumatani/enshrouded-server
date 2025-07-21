0e68100289d4ac5b50299393f11380b4cd5b5c6e
kodaikumatani <koudai.aco5423@gmail.com>
2025-07-21 22:45:37 +0900

    feat: gild command を追加

---
+++ discord/application-command.js
@@ -0,0 +1,36 @@
+import { REST, Routes, ApplicationCommandType } from 'discord.js';
+
+const commands = [
+  {
+    name: "server",
+    type: ApplicationCommandType.ChatInput,
+    description: "enshrouded server controller",
+    options: [
+      {
+        name: "action",
+        type: ApplicationCommandType.Message,
+        description: "GCE Instance start/stop",
+        required: true,
+        choices: [
+          { name: "start", value: "stop" },
+          { name: "stop", value: "stop" },
+        ]
+      },
+    ]
+  }
+];
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+try {
+  console.log('Started refreshing application (/) commands.');
+
+  await rest.put(
+    Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+    { body: commands }
+  );
+
+  console.log('Successfully reloaded application (/) commands.');
+} catch (error) {
+  console.error(error);
+}
