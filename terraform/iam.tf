84a3eb1ee8d64ff96bf1185a1ebe059479714a98
kodaikumatani <koudai.aco5423@gmail.com>
2025-07-21 11:16:53 +0900

    feat: function deploy サービスアカウントを作成

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,4 +1,29 @@
 ####################################
+# Create Service Account
+####################################
+resource "google_service_account" "deployer" {
+  account_id   = "cloud-functions-deployer"
+  display_name = "GitHub Actions Cloud Functions Deployer"
+}
+
+####################################
+# IAM policy for projects
+####################################
+module "project-iam-bindings" {
+  source   = "terraform-google-modules/iam/google//modules/projects_iam"
+  version  = "~> 8.0"
+
+  projects = [var.project_id]
+  mode     = "additive"
+
+  bindings = {
+    "roles/cloudfunctions.developer" = [
+      "serviceAccount:${google_service_account.deployer.email}"
+    ]
+  }
+}
+
+####################################
 # IAM policy for Cloud Run Service
 ####################################
 resource "google_cloud_run_service_iam_member" "member" {
