6b44d2e889e3c304f5d6567fbbbd15f8094c21ac
kodaikumatani <koudai.aco5423@gmail.com>
2025-07-26 21:29:13 +0900

    wip

--- terraform/functions.tf
+++ terraform/functions.tf
@@ -8,8 +8,8 @@ resource "google_cloudfunctions2_function" "default" {
     entry_point = "HelloHTTP" # Set the entry point
     source {
       storage_source {
-        bucket = google_storage_bucket.default.name
-        object = google_storage_bucket_object.object.name
+        bucket = google_storage_bucket.go124.name
+        object = google_storage_bucket_object.go124.name
       }
     }
   }
@@ -40,6 +40,44 @@ resource "google_cloudfunctions2_function" "default" {
   }
 }
 
-output "function_uri" {
-  value = google_cloudfunctions2_function.default.service_config[0].uri
+resource "google_cloudfunctions2_function" "discord-interactions" {
+  name        = "discord-interactions"
+  location    = "us-central1"
+  description = "Discord Interactions webhook."
+
+  build_config {
+    runtime     = "nodejs22"
+    entry_point = "helloHttp" # Set the entry point
+    source {
+      storage_source {
+        bucket = google_storage_bucket.nodejs22.name
+        object = google_storage_bucket_object.nodejs22.name
+      }
+    }
+  }
+
+  service_config {
+    timeout_seconds       = 60
+    service_account_email = google_service_account.runner.email
+    environment_variables = {
+      CLIENT_PUBLIC_KEY = "c0f9c3f75e45e1f5a69a6a18055c27636ca093df4b56250da90eaf75d5e28d68"
+      PROJECT_ID        = var.project_id
+      ZONE              = "asia-east1-a"
+      INSTANCE          = "instance-20250714-084552"
+    }
+    secret_environment_variables {
+      key        = "WEBHOOK_URL"
+      project_id = var.project_id
+      secret     = module.secret-manager.env_vars.SECRET.secret
+      version    = "latest"
+    }
+  }
+
+  lifecycle {
+    ignore_changes = [
+      build_config.0.entry_point,
+      build_config.0.source,
+      build_config.0.docker_repository,
+    ]
+  }
 }
