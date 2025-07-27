9f0345f4a53bb304f12007b7714129801592a2fd
kodaikumatani <koudai.aco5423@gmail.com>
2025-07-27 20:56:07 +0900

    feat: discord-interactions で Pub/Sub に message を push する

---
+++ functions/discord-interactions/function.js
@@ -0,0 +1,46 @@
+import functions from '@google-cloud/functions-framework';
+import {
+  InteractionResponseType,
+  InteractionType,
+  verifyKey,
+} from 'discord-interactions';
+import { PubSub } from '@google-cloud/pubsub';
+
+const clientPublicKey = process.env.CLIENT_PUBLIC_KEY;
+const projectID = process.env.PROJECT_ID
+const topicName = process.env.TOPIC_NAME
+const pubsub = new PubSub({PROJECT_ID: projectID});
+
+functions.http('discordInteractions', async (req, res) => {
+  // Verify the request
+  const signature = req.get('X-Signature-Ed25519');
+  const timestamp = req.get('X-Signature-Timestamp');
+  const isValidRequest = await verifyKey(
+    req.rawBody,
+    signature,
+    timestamp,
+    clientPublicKey,
+  );
+  if (!isValidRequest) {
+    return res.status(401).end('Bad request signature');
+  }
+
+  // Handle the payload
+  const interaction = req.body;
+
+  if (interaction && interaction.type === InteractionType.APPLICATION_COMMAND) {
+    const command = interaction.data.options?.[0]?.value; // "start" or "stop"
+    await pubsub.topic(topicName).publishMessage({data: Buffer.from(command)})
+
+    res.send({
+      type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+      data: {
+        content: `Your request to ${command} the server has been received.`,
+      },
+    });
+  } else {
+    res.send({
+      type: InteractionResponseType.PONG,
+    });
+  }
+});
